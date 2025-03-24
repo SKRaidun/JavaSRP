aad30531ba64614d5f4499fa7bcb0d7092e22f4b
SKRaidun <163428996+SKRaidun@users.noreply.github.com>
2025-03-24 17:11:41 +0300

    Add task 3.4

--- src/main/java/FraudDetector.java
+++ src/main/java/FraudDetector.java
@@ -1,7 +1,17 @@
 public class FraudDetector {
 
+    private FraudRule rule1 = new Rule1();
+    private FraudRule rule2 = new Rule2();
+    private FraudRule rule3 = new Rule3();
+    private FraudRule rule4 = new Rule4();
+    private FraudRule rule5 = new Rule5();
+
     boolean isFraud(Transaction transaction) {
-        return true;
+        return rule1.isFraud(transaction)
+                || rule2.isFraud(transaction)
+                || rule3.isFraud(transaction)
+                || rule4.isFraud(transaction)
+                || rule5.isFraud(transaction);
     }
 
 }
